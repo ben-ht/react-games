4eab6a510d3dd1f16efb58b8545f1c28cf903dc1
unknown <max.pochet@orange.fr>
2024-04-18 14:51:15 +0200

    feat(register login): add login register

---
+++ src/components/Register/index.spec.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+
+import Register from "./index";
+
+describe("Register", () => {
+  it("It renders"),
+    () => {
+      render(<Register />);
+      expect(
+        screen.getAllByText(new RegExp("Register Component", "i"))[0]
+      ).toBeInTheDocument();
+    };
+});
