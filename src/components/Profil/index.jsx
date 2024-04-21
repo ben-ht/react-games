cc2c3c7095ecba40ed4a6583f63a49fea5a20524
unknown <max.pochet@orange.fr>
2024-04-21 15:09:18 +0200

    fix(profil): delete cover and add message no fav

--- src/components/Profil/index.jsx
+++ src/components/Profil/index.jsx
@@ -4,23 +4,21 @@ import Meta from "antd/es/card/Meta";
 
 export default function Profil() {
   const { user } = useUser();
+  console.log(user.favorites);
   return (
     <div>
       <h1>Hello {user.name}</h1>
       <h2>Here is your favorites games list</h2>
       <div className="top-games-container">
-        {user.favorites
-          ? user.favorites.map((game) => (
-              <Card
-                key={game.id}
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="Game cover" src={""} />}
-              >
-                <Meta title={game.name} description={game.storyline} />
-              </Card>
-            ))
-          : null}
+        {user.favorites && user.favorites.length !== 0 ? (
+          user.favorites.map((game) => (
+            <Card key={game.id} hoverable style={{ width: 240 }}>
+              <Meta title={game.name} description={game.storyline} />
+            </Card>
+          ))
+        ) : (
+          <p>No game add to favorite</p>
+        )}
       </div>
     </div>
   );
