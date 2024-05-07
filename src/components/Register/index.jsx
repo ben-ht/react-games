4afff34fd5c2fffb0f129ef65b691f44b68dbbb9
Benjamin Hembert <ben.hbt62@gmail.com>
2024-05-07 15:55:48 +0200

    Fixed css and wording

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { createUser } from '../../business/user';
 import { Link } from 'react-router-dom';
-import './index.css';
 import useJwt from '../../hooks/useJwt';
 import { useNavigate } from 'react-router-dom/dist';
 
